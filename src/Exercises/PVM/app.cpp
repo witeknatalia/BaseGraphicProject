2a905586d7f83de87d9be14743859d287cf68c57
Natalia WItek <nw3346@gmail.com>
2021-01-20 17:39:02 +0100

    zad PVM

--- src/Exercises/PVM/app.cpp
+++ src/Exercises/PVM/app.cpp
@@ -55,7 +55,7 @@ void SimpleShapeApplication::init() {
         glUniformBlockBinding(program, u_modifiers_index, 0);
     }
 
-    GLuint ubo_handle(0u);
+    GLuint ubo_handle(0);
     glGenBuffers(1,&ubo_handle);
     glBindBuffer(GL_UNIFORM_BUFFER, ubo_handle);
     glBufferData(GL_UNIFORM_BUFFER, 8 * sizeof(float), nullptr, GL_STATIC_DRAW);
@@ -71,7 +71,7 @@ void SimpleShapeApplication::init() {
     GLuint v_buffer_handle;
     glGenBuffers(1, &v_buffer_handle);
     glBindBuffer(GL_ARRAY_BUFFER, v_buffer_handle);
-    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), vertices.data(), GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat), vertices.data(), GL_STATIC_DRAW);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 
@@ -89,13 +89,21 @@ void SimpleShapeApplication::init() {
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
+
     auto u_transform_index = glGetUniformBlockIndex(program, "Transformations");
     if (u_transform_index == GL_INVALID_INDEX)
     {
-        std::cout << "Cannot find Modifiers uniform block in program" << std::endl;
+        std::cout << "Cannot find Transformations uniform block in program" << std::endl;
     } else {
         glUniformBlockBinding(program, u_transform_index, 1);
     }
+
+    GLuint u_buffer_handle;
+    glGenBuffers(2, &u_buffer_handle);
+    glBindBuffer(GL_ARRAY_BUFFER, u_buffer_handle);
+
+
+
 
     int w, h;
     std::tie(w, h) = frame_buffer_size();
@@ -113,7 +121,7 @@ void SimpleShapeApplication::init() {
     auto PVM = P * V * M;
     
     glBufferData(GL_UNIFORM_BUFFER, sizeof(glm::mat4), nullptr, GL_STATIC_DRAW);
-    glBufferSubData(GL_UNIFORM_BUFFER, 1, sizeof(glm::mat4), &PVM[0]);
+    glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(glm::mat4), &PVM[0]);
 
 
     glClearColor(0.81f, 0.81f, 0.8f, 1.0f);
